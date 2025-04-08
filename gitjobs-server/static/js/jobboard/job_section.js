b14d6dbb8b3aa93723521934e8e963b235a6ba48
Cintia Sánchez García <cynthiasg@icloud.com>
2025-04-08 13:15:51 +0200

    Links to share jobs postings (#230)
    Closes #177
    
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- gitjobs-server/static/js/jobboard/job_section.js
+++ gitjobs-server/static/js/jobboard/job_section.js
@@ -80,3 +80,49 @@ export const copyEmbedCodeToClipboard = (elId) => {
 
   showSuccessAlert("Embed code copied to clipboard!");
 };
+
+export const shareJob = () => {
+  const socialLinks = document.getElementById("social-links");
+  const jobId = socialLinks.dataset.jobId;
+  const shareUrl = `${window.location.origin}?job_id=${jobId}`;
+  const subject = "Check out this job I found on GitJobs!";
+  const message = "Check out this job I found on GitJobs!";
+
+  const anchorTags = socialLinks.querySelectorAll("a");
+  anchorTags.forEach((anchorTag) => {
+    const platform = anchorTag.dataset.platform;
+
+    switch (platform) {
+      case "twitter":
+        anchorTag.setAttribute(
+          "href",
+          `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}&url=${shareUrl}`,
+        );
+        break;
+      case "facebook":
+        anchorTag.setAttribute(
+          "href",
+          `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&quote=${encodeURIComponent(message)}`,
+        );
+        break;
+      case "linkedin":
+        anchorTag.setAttribute("href", `https://www.linkedin.com/sharing/share-offsite/?url=${shareUrl}`);
+        break;
+      case "email":
+        anchorTag.setAttribute(
+          "href",
+          `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
+            `${message} ${shareUrl}`,
+          )}`,
+        );
+        break;
+    }
+  });
+
+  // Copy link to clipboard
+  const copyLink = document.querySelector("#copy-link");
+  copyLink.addEventListener("click", () => {
+    navigator.clipboard.writeText(shareUrl);
+    showSuccessAlert("Job link copied to clipboard!");
+  });
+};
