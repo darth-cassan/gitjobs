6243b86931a328e7e3982970fc837457aa0b2cf6
Daniel Castaño Sánchez <danielcastanosanchez@icloud.com>
2025-09-03 08:28:07 +0200

    Fix e2e tests
    Signed-off-by: Daniel Castaño Sánchez <danielcastanosanchez@icloud.com>

--- tests/e2e/playwright.spec.ts
+++ tests/e2e/playwright.spec.ts
@@ -2,10 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test.describe('GitJobs', () => {
   test.beforeEach(async ({ page }) => {
-    await page.route('https://cmp.osano.com/16A0DbT9yDNIaQkvZ/cac8d053-b9a4-4677-8ab3-cb6720944467/osano.js', route => route.abort());
     for (let i = 0; i < 3; i++) {
       try {
-        await page.goto('/', { timeout: 60000 });
+        await page.goto('http://127.0.0.1:8080/', { timeout: 60000 });
         break;
       } catch (error) {
         console.log(`Failed to navigate to page, retrying... (${i + 1}/3)`);
@@ -93,11 +92,8 @@ test.describe('GitJobs', () => {
 
   test('should log in a user', async ({ page }) => {
     await page.locator('#user-dropdown-button').click();
-    await page.route('https://cmp.osano.com/16A0DbT9yDNIaQkvZ/cac8d053-b9a4-4677-8ab3-cb6720944467/osano.js', route => route.abort());
     await page.getByRole('link', { name: 'Log in' }).click();
-    await page.route('https://cmp.osano.com/16A0DbT9yDNIaQkvZ/cac8d053-b9a4-4677-8ab3-cb6720944467/osano.js', route => route.abort());
     await page.waitForURL('**/log-in');
-    await page.route('https://cmp.osano.com/16A0DbT9yDNIaQkvZ/cac8d053-b9a4-4677-8ab3-cb6720944467/osano.js', route => route.abort());
     await page.locator('#username').fill('test');
     await page.locator('#password').fill('test');
     await page.getByRole('button', { name: 'Submit' }).click();
@@ -107,7 +103,6 @@ test.describe('GitJobs', () => {
     // Log in first
     await page.locator('#user-dropdown-button').click();
     await page.getByRole('link', { name: 'Log in' }).click();
-    await page.route('https://cmp.osano.com/**', route => route.abort());
     await page.waitForURL('**/log-in');
     await page.locator('#username').fill('test');
     await page.locator('#password').fill('test');
